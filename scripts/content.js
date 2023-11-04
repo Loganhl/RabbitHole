f10e9a4e9b585ab68c15405e7d510d6454e15bd2
crewskaelyn <crewskaelyn@gmail.com>
2023-11-03 23:53:48 -0500

    flask

--- scripts/content.js
+++ scripts/content.js
@@ -18,7 +18,24 @@ function setupContextMenu() {
         data: { value: data.selectionText }
       });
       
-  
+      const highlightedText = data.selectionText;
+
+        // Send the highlighted text to your Flask server
+        fetch('http://localhost:5000/highlight', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ highlighted_text: highlightedText }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response from the server, e.g., update the side panel with the result
+                document.body.querySelector('#definition-word').innerText = 'Highlighted Text';
+                document.body.querySelector('#definition-text').innerText = data.result;
+                chrome.sidePanel.open({ tabId: tab.id });
+            });
+      
       // Open the side panel using chrome.sidePanel.open()
       chrome.sidePanel.open({ tabId: tab.id });
     }
