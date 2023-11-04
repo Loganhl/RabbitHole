9e7dbe173f40d8ee7d743f76d0b4320da9162f0f
Liam <lgreim33@gmail.com>
2023-11-03 23:20:30 -0500

    URLs and Article names are now stored in listed pairs within a list

--- newsLitApi.py
+++ newsLitApi.py
@@ -1,14 +1,18 @@
 import requests
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 import json
+
 
 #search will hold the keywords we want to search google news for
 search = str()
+#list variables to hold the parsed data
+headers_list = list()
+link_list = list()
+relevant_articles = list()
 
-search = "werewolves"
+search = "cats"
 
-#holds the request information from the query in a bunary format
-
+#holds the request information from the query in a binary format
 response = requests.get("https://www.google.com/search?q="+ search +"&tbm=nws")
     
 if response.status_code == 200:
@@ -19,28 +23,20 @@ if response.status_code == 200:
 
     #get all headers(article titles)
     heading_object=soup.find_all( 'h3' )
-    
-
-    #will hold the links to the articles
-    link_list = list()
-    
-    relevant_articles = list()
 
 
+    #parse headers and urls into seperate lists
+    for info in heading_object: 
+        headers_list.append(info.getText()) 
+        
     for link in soup.find_all("a"):
         link_list.append(link.get("href").replace("/url?q=",""))
+       
+    #last 7 urls aren't search results, nor are the first 16 elements
+    link_list = link_list[16:-7]
+    
+    #zip header_list elements and link_lsit
+    relevant_articles.extend([list(a) for a in zip(headers_list,link_list)]) 
         
-        
-    #for info,link in heading_object,soup.find_all("a"):
-    #   relevant_articles.append()
-        
-        
-        
-        
-    print(link_list)
-            
-
-    for info in heading_object: 
-        print(info.getText()) 
-        print("------") 
-
+    for pair in relevant_articles:
+        print(pair,"\n\n")
