ad0914013873530f98ac41867cf15c45817a6f6b
Liam <lgreim33@gmail.com>
2023-11-03 22:35:33 -0500

    Added Comments

--- newsLitApi.py
+++ newsLitApi.py
@@ -5,27 +5,42 @@ import json
 #search will hold the keywords we want to search google news for
 search = str()
 
-search = "wolves"
+search = "werewolves"
 
-#holds the request information from the queried 
-raw = requests.get("https://www.google.com/search?q="+ search +"&tbm=nws")
+#holds the request information from the query in a bunary format
 
-
-soup = BeautifulSoup(raw.text, 
-                         "html.parser") 
-
-heading_object=soup.find_all( 'h3' )
-
-link_list = list()
-
-for link in soup.find_all("a"):
-    link_list.append(link.get("href"))
+response = requests.get("https://www.google.com/search?q="+ search +"&tbm=nws")
     
+if response.status_code == 200:
+    #parse the response into html
+    soup = BeautifulSoup(response.text, 
+                            "html.parser") 
+
+
+    #get all headers(article titles)
+    heading_object=soup.find_all( 'h3' )
     
-print(link_list)
+
+    #will hold the links to the articles
+    link_list = list()
+    
+    relevant_articles = list()
+
+
+    for link in soup.find_all("a"):
+        link_list.append(link.get("href").replace("/url?q=",""))
         
+        
+    #for info,link in heading_object,soup.find_all("a"):
+    #   relevant_articles.append()
+        
+        
+        
+        
+    print(link_list)
+            
 
-for info in heading_object: 
-    print(info.getText()) 
-    print("------") 
+    for info in heading_object: 
+        print(info.getText()) 
+        print("------") 
 
