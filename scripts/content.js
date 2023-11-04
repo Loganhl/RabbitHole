edafb35bc782c045703f58ece1973106276297db
crewskaelyn <crewskaelyn@gmail.com>
2023-11-03 23:02:35 -0500

    extension

---
+++ scripts/content.js
@@ -0,0 +1,26 @@
+function setupContextMenu() {
+    chrome.contextMenus.create({
+      id: 'define-word',
+      title: 'Define',
+      contexts: ['selection']
+    });
+  }
+  
+  chrome.runtime.onInstalled.addListener(() => {
+    setupContextMenu();
+  });
+  
+  chrome.contextMenus.onClicked.addListener((data, tab) => {
+    if (data.menuItemId === 'define-word') {
+      // Open the side panel when the "Define" option is clicked.
+      chrome.runtime.sendMessage({
+        name: 'define-word',
+        data: { value: data.selectionText }
+      });
+      
+  
+      // Open the side panel using chrome.sidePanel.open()
+      chrome.sidePanel.open({ tabId: tab.id });
+    }
+  });
+  
